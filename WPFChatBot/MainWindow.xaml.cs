fb02eb6c09de39d9b348d5dd8752fe13bf492f42
sevilsariyeva00 <sevilsariyeva00@gmail.com>
2023-02-28 03:18:20 +0400

    Add project files.

---
+++ WPFChatBot/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFChatBot
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void startBtn_Click(object sender, RoutedEventArgs e)
+        {
+            mainGrid.Children.Clear();
+            mainGrid.Background = null;
+            //elave xaml ach duzelt sonra code'u copy edib xaml'i silersen
+        }
+    }
+}
